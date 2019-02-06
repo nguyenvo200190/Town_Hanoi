b5d309ba3389232e1fd3e37c7b11f6c0bc621870
NguyenVo <nvo3@uncc.edu>
2019-02-06 11:24:55 +0700

    Town Hanoi

---
+++ node.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  2 11:29:16 2019
+
+@author: vopha
+"""
+
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+    
+  def get_next_node(self):
+    return self.next_node
+  
+  def get_value(self):
+    return self.value
